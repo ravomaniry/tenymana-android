65d395f42c50f9faa67bd350dfc89f8e6c3fad8c
Ravomaniry <arm@e2f.com>
2020-10-17 08:55:09 +0300

    noop

--- app/src/test/java/mg/maniry/tenymana/game/sharedLogic/grid/matchTest.kt
+++ app/src/test/java/mg/maniry/tenymana/game/sharedLogic/grid/matchTest.kt
@@ -1,7 +1,11 @@
 package mg.maniry.tenymana.game.sharedLogic.grid
 
 import com.google.common.truth.Truth.assertThat
-import mg.maniry.tenymana.game.models.*
+import mg.maniry.tenymana.game.models.BibleVerse
+import mg.maniry.tenymana.game.models.Character
+import mg.maniry.tenymana.game.models.Grid
+import mg.maniry.tenymana.game.models.Move
+import mg.maniry.tenymana.game.models.Point.Companion.directions
 import org.junit.Test
 
 class MatchTest {
@@ -15,18 +19,18 @@ class MatchTest {
                 listOf(c('g'), c('f'), null)
             )
         )
-        assertThat(grid.firstVisibleMatch(words, 2, Point.directions)).isEqualTo(Move.xy(0, 0, 2, 0))
+        assertThat(grid.firstVisibleMatch(words, 2, directions)).isEqualTo(Move.xy(0, 0, 2, 0))
         // [0] is resolved
         words[0] = words[0].resolvedVersion
-        assertThat(grid.firstVisibleMatch(words, 2, Point.directions)).isEqualTo(Move.xy(2, 1, 1, 1))
+        assertThat(grid.firstVisibleMatch(words, 2, directions)).isEqualTo(Move.xy(2, 1, 1, 1))
         // [0], [2] resolved + off grid
         words[2] = words[2].resolvedVersion
-        assertThat(grid.firstVisibleMatch(words, 2, Point.directions)).isNull()
+        assertThat(grid.firstVisibleMatch(words, 2, directions)).isNull()
         // [0], [2] resolved on grid
-        assertThat(grid.firstVisibleMatch(words, 3, Point.directions)).isEqualTo(Move.xy(1, 2, 0, 2))
+        assertThat(grid.firstVisibleMatch(words, 3, directions)).isEqualTo(Move.xy(1, 2, 0, 2))
         // [0], [2], [4] resolved
         words[4] = words[4].resolvedVersion
-        assertThat(grid.firstVisibleMatch(words, 4, Point.directions)).isNull()
+        assertThat(grid.firstVisibleMatch(words, 4, directions)).isNull()
     }
 
     @Test
@@ -40,9 +44,9 @@ class MatchTest {
             )
         )
         // ABC off grid
-        assertThat(grid.firstVisibleMatch(words, 2, Point.directions)).isEqualTo(Move.xy(1, 1, 1, 0))
+        assertThat(grid.firstVisibleMatch(words, 2, directions)).isEqualTo(Move.xy(1, 1, 1, 0))
         // ABC in grid
-        assertThat(grid.firstVisibleMatch(words, 3, Point.directions)).isEqualTo(Move.xy(0, 0, 0, 2))
+        assertThat(grid.firstVisibleMatch(words, 3, directions)).isEqualTo(Move.xy(0, 0, 0, 2))
     }
 
     private fun c(v: Char) = Character(v.toUpperCase(), v.toLowerCase())
