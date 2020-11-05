ab00179ae856bd448062f043dba4d08f37746a18
Ravomaniry <arm@e2f.com>
2020-11-05 12:21:18 +0300

    Deleted unused mock files

--- app/src/androidTest/java/mg/maniry/tenymana/helpers/mockito.kt
+++ app/src/androidTest/java/mg/maniry/tenymana/helpers/mockito.kt
@@ -1,8 +1,16 @@
 package mg.maniry.tenymana.helpers
 
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import org.mockito.Mockito
 import org.mockito.stubbing.OngoingStubbing
 
 fun <T> whenever(methodCall: T): OngoingStubbing<T> {
     return Mockito.`when`(methodCall)
 }
+
+fun <T> verifyOnce(tested: T): T = verify(tested, times(1))
+
+fun <T> verifyNever(tested: T): T = verify(tested, times(0))
+
+fun <T> verifyTimes(tested: T, n: Int): T = verify(tested, times(n))
