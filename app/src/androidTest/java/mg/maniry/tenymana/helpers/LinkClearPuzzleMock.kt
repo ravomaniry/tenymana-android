65009713b7d0a180454df222ac413e9b0293c2a2
Ravomaniry <arm@e2f.com>
2020-11-06 09:33:46 +0300

    Fixed broken test

--- app/src/androidTest/java/mg/maniry/tenymana/helpers/LinkClearPuzzleMock.kt
+++ app/src/androidTest/java/mg/maniry/tenymana/helpers/LinkClearPuzzleMock.kt
@@ -2,6 +2,7 @@ package mg.maniry.tenymana.helpers
 
 import androidx.lifecycle.MutableLiveData
 import mg.maniry.tenymana.gameLogic.linkClear.LinkClearPuzzle
+import mg.maniry.tenymana.gameLogic.linkClear.SolutionItem
 import mg.maniry.tenymana.gameLogic.models.*
 
 class LinkClearPuzzleMock(
@@ -14,6 +15,7 @@ class LinkClearPuzzleMock(
     override var cleared: List<Point>? = null
     override val score = MutableLiveData(0)
     override var completed = false
+    override var solution = emptyList<SolutionItem<Character>>()
 
     override fun propose(move: Move) = proposeFn(move)
     override fun undo() = undoFn()
