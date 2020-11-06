4751cedc6bb035a45ef83ae18a5cacb907950966
Ravomaniry <arm@e2f.com>
2020-11-06 18:18:30 +0300

    Cleared cells animation view

--- app/src/main/java/mg/maniry/tenymana/ui/game/puzzle/views/GridClearedView.kt
+++ app/src/main/java/mg/maniry/tenymana/ui/game/puzzle/views/GridClearedView.kt
@@ -1,2 +1,179 @@
 package mg.maniry.tenymana.ui.game.puzzle.views
 
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import mg.maniry.tenymana.gameLogic.models.Point
+import java.util.*
+
+class GridClearedView : View {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) :
+            super(context, attributeSet, defStyleAttr)
+
+    private val animator = ValueAnimator.ofFloat(0f, 1f).apply {
+        duration = animDuration
+    }
+    private val control = GridClearedViewControl(animator)
+
+    fun onSettingsChanged(settings: DrawingSettings) {
+        control.settings = settings
+    }
+
+    fun onValue(value: List<Point>?) {
+        control.onValue(value)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        if (canvas != null) {
+            control.onDraw(canvas)
+        }
+    }
+
+    init {
+        val t0 = Date().time
+        animator.addUpdateListener {
+            val t = (Date().time - t0).toDouble() / animDuration
+            val reRender = control.onTick(t)
+            if (reRender) {
+                invalidate()
+            }
+        }
+    }
+
+    companion object {
+        const val animDuration = 500L
+    }
+}
+
+class GridClearedViewControl(
+    private val animator: ValueAnimator
+) {
+    enum class Mode { GROW, IDLE, SHRINK }
+
+    var t: Double = 0.0
+    var settings: DrawingSettings? = null
+    private var sizes: MutableList<Float> = mutableListOf()
+    private var centers: List<Point> = mutableListOf()
+    private var tZeroes: MutableList<Double> = mutableListOf()
+    private var mode: Mode? = null
+    private var value: List<Point>? = null
+    private val cellSize: Float get() = settings?.charGridCellSize ?: 0f
+    private val paint = Paint().apply {
+        style = Paint.Style.FILL
+        color = Color.BLUE
+    }
+
+    fun onValue(value: List<Point>?) {
+        this.value = value
+        stopAnimator()
+        if (value != null) {
+            calcStarts()
+            initSizes()
+            initCenters()
+            animator.start()
+        }
+    }
+
+    fun onTick(t: Double): Boolean {
+        this.t = t
+        val nextMode = when {
+            t < .5 -> Mode.GROW
+            t <= 0.8 -> Mode.IDLE
+            t < 1 -> Mode.SHRINK
+            else -> null
+        }
+        if ((mode == Mode.IDLE && nextMode == Mode.IDLE) || mode == null && nextMode == null) {
+            return false
+        }
+        mode = nextMode
+        return true
+    }
+
+    fun onDraw(canvas: Canvas) {
+        if (settings != null) {
+            when (mode) {
+                Mode.GROW -> handleSizeGrow()
+                Mode.IDLE -> handleSizeIdle()
+                Mode.SHRINK -> handleSizeShrink()
+            }
+            if (mode != null) {
+                draw(canvas)
+            }
+        }
+    }
+
+    private fun stopAnimator() {
+        if (animator.isRunning) {
+            animator.cancel()
+        }
+    }
+
+    private fun calcStarts() {
+        tZeroes = mutableListOf()
+        val interval = maxDelay / (value!!.size - 1)
+        for (i in value!!.indices) {
+            tZeroes.add(i * interval)
+        }
+    }
+
+    private fun initSizes() {
+        sizes = MutableList(value!!.size) { 0f }
+    }
+
+    private fun initCenters() {
+        val origin = settings?.charGridOrigin
+        if (origin != null) {
+            centers = value!!.map {
+                val x = origin.x + (it.x.toFloat() + 0.5) * cellSize
+                val y = origin.y - (it.y.toFloat() + 0.5) * cellSize
+                Point(x.toInt(), y.toInt())
+            }
+        }
+    }
+
+    private fun handleSizeGrow() {
+        for (i in sizes.indices) {
+            val t0 = tZeroes[i]
+            sizes[i] = (cellSize * (t - t0) / (maxGrow - t0)).toFloat()
+        }
+    }
+
+    private fun handleSizeIdle() {
+        for (i in sizes.indices) {
+            sizes[i] = cellSize
+        }
+    }
+
+    private fun handleSizeShrink() {
+        for (i in sizes.indices) {
+            sizes[i] = cellSize - (cellSize * (t - shrink0) / (1 - shrink0)).toFloat()
+        }
+    }
+
+    private fun draw(canvas: Canvas) {
+        for (i in sizes.indices) {
+            if (sizes[i] > 0) {
+                canvas.drawRect(
+                    centers[i].x - sizes[i] / 2,
+                    centers[i].y - sizes[i] / 2,
+                    centers[i].x + sizes[i] / 2,
+                    centers[i].y + sizes[i] / 2,
+                    paint
+                )
+            }
+        }
+    }
+
+    companion object {
+        const val maxDelay = 0.25
+        const val maxGrow = 0.5
+        const val shrink0 = 0.8
+    }
+}
--- app/src/test/java/mg/maniry/tenymana/ui/game/puzzle/views/GridClearedViewTest.kt
+++ app/src/test/java/mg/maniry/tenymana/ui/game/puzzle/views/GridClearedViewTest.kt
@@ -1,4 +1,139 @@
 package mg.maniry.tenymana.ui.game.puzzle.views
 
+import android.animation.ValueAnimator
+import android.graphics.Canvas
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.*
+import mg.maniry.tenymana.gameLogic.models.Point
+import mg.maniry.tenymana.utils.TestRect
+import mg.maniry.tenymana.utils.verifyNever
+import mg.maniry.tenymana.utils.verifyOnce
+import org.junit.Test
+
 class GridClearedViewTest {
+    @Test
+    fun simpleAnim() {
+        // canvas
+        // Add value and draw
+        val rects = mutableListOf<TestRect>()
+        val canvas: Canvas = mock {
+            on { drawRect(any(), any(), any(), any(), any()) } doAnswer {
+                rects.add(TestRect.fromMock(it.arguments)); Unit
+            }
+        }
+
+        fun GridClearedViewControl.reRender(t: Double) {
+            val should = onTick(t)
+            if (should) {
+                onDraw(canvas)
+            }
+        }
+        // animator
+        var isRunning = false
+        val animator: ValueAnimator = mock {
+            on { this.isRunning } doAnswer { isRunning }
+            on { start() } doAnswer {
+                isRunning = true; Unit
+            }
+            on { cancel() } doAnswer {
+                isRunning = false; Unit
+            }
+        }
+        // control
+        val control = GridClearedViewControl(animator)
+        val cellSize = 20f
+        val x0 = 10f
+        val y0 = 100f
+        control.settings = DrawingSettings().apply {
+            charGridCellSize = cellSize
+            charGridOrigin = Point(x0.toInt(), y0.toInt())
+        }
+        // on null value does nothing
+        control.onValue(null)
+        verifyNever(animator).cancel()
+        verifyNever(animator).start()
+        // positive value
+        control.onValue(listOf(Point(0, 0), Point(1, 1)))
+        verifyNever(animator).cancel()
+        verifyOnce(animator).start()
+        // positive again stops and then starts
+        clearInvocations(animator)
+        control.onValue(listOf(Point(0, 0), Point(1, 1)))
+        verifyOnce(animator).cancel()
+        verifyOnce(animator).start()
+        // Null stops
+        clearInvocations(animator)
+        control.onValue(null)
+        verifyOnce(animator).cancel()
+        verifyNever(animator).start()
+
+        control.onValue(listOf(Point(0, 0), Point(1, 1)))
+        control.apply {
+            onTick(0.0)
+            onDraw(canvas)
+        }
+        assertThat(rects).isEmpty()
+        // t = 0.1: draw cells[0] = 4, cells[1] = 0
+        control.reRender(0.1)
+        assertThat(rects).isEqualTo(listOf(TestRect.xywh(20f - 2, 90f - 2, 4f, 4f)))
+        // t = 0.25: cells[0] = 10
+        rects.removeAll { true }
+        control.reRender(0.25)
+        assertThat(rects).isEqualTo(listOf(TestRect.xywh(20f - 5, 90f - 5, 10f, 10f)))
+        // t = 0.3: cells[0] = 12; cells[1] = 4
+        rects.removeAll { true }
+        control.reRender(0.3)
+        assertThat(rects).isEqualTo(
+            listOf(
+                TestRect.xywh(20f - 6, 90f - 6, 12f, 12f),
+                TestRect.xywh(40f - 2, 70f - 2, 4f, 4f)
+            )
+        )
+        rects.removeAll { true }
+        // t = 0.4: cells[0] = 16, cells[1] = 12
+        control.reRender(0.4)
+        assertThat(rects).isEqualTo(
+            listOf(
+                TestRect.xywh(20f - 8, 90f - 8, 16f, 16f),
+                TestRect.xywh(40f - 6, 70f - 6, 12f, 12f)
+            )
+        )
+        rects.removeAll { true }
+        // t = 0.5: cells[0] = 20, cells[1] = 20
+        control.reRender(0.5)
+        assertThat(rects).isEqualTo(
+            listOf(
+                TestRect.xywh(20f - 10, 90f - 10, 20f, 20f),
+                TestRect.xywh(40f - 10, 70f - 10, 20f, 20f)
+            )
+        )
+        // onTick till 0.8 does nothing
+        clearInvocations(canvas)
+        rects.removeAll { true }
+        control.reRender(0.6)
+        control.reRender(0.8)
+        verifyZeroInteractions(canvas)
+        // shrink
+        // t=0.9; size = 10
+        control.reRender(0.9)
+        assertThat(rects).isEqualTo(
+            listOf(
+                TestRect.xywh(20f - 5, 90f - 5, 10f, 10f),
+                TestRect.xywh(40f - 5, 70f - 5, 10f, 10f)
+            )
+        )
+        // t=95; size = 5
+        rects.removeAll { true }
+        control.reRender(0.95)
+        assertThat(rects).isEqualTo(
+            listOf(
+                TestRect.xywh(20f - 2.5f, 90f - 2.5f, 5f, 5f),
+                TestRect.xywh(40f - 2.5f, 70f - 2.5f, 5f, 5f)
+            )
+        )
+        // t=1; size = 0
+        clearInvocations(canvas)
+        control.reRender(1.0)
+        verifyZeroInteractions(canvas)
+    }
 }
