f0dd246f3ede9211fa3867218e6b67d88ff50793
Ravomaniry <arm@e2f.com>
2020-11-07 20:06:07 +0300

    Updated Paths screen to use ListAdapter

--- app/src/main/java/mg/maniry/tenymana/ui/game/paths/PathAdapter.kt
+++ app/src/main/java/mg/maniry/tenymana/ui/game/paths/PathAdapter.kt
@@ -3,6 +3,8 @@ package mg.maniry.tenymana.ui.game.paths
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import mg.maniry.tenymana.R
 import mg.maniry.tenymana.databinding.PathsScreenVerseItemBinding
@@ -11,71 +13,61 @@ import mg.maniry.tenymana.repositories.models.Path
 
 class PathAdapter(
     verseClickHandler: (Int) -> Unit
-) : RecyclerView.Adapter<PathViewHolder>() {
+) : ListAdapter<PathVerseItem, PathAdapter.ViewHolder>(DiffUtilCb()) {
     private val onClick = OnVerseClick(verseClickHandler)
 
     var scores: List<Score>? = null
         set(value) {
             field = value
-            notifyDataSetChanged()
+            submitList(path?.toVerseItems(value))
         }
 
     var path: Path? = null
         set(value) {
             field = value
-            notifyDataSetChanged()
+            submitList(value?.toVerseItems(scores))
         }
 
-    override fun getItemCount(): Int {
-        return when (path) {
-            null -> 0
-            else -> path!!.end - path!!.start + 1
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), onClick)
+    }
+
+    class DiffUtilCb : DiffUtil.ItemCallback<PathVerseItem>() {
+        override fun areItemsTheSame(oldItem: PathVerseItem, newItem: PathVerseItem) =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: PathVerseItem, newItem: PathVerseItem) =
+            oldItem == newItem
+    }
+
+    class ViewHolder private constructor(
+        private val binding: PathsScreenVerseItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: PathVerseItem, onClick: OnVerseClick) {
+            binding.item = item
+            binding.onClick = onClick
+            binding.background = when (item.score) {
+                0 -> R.drawable.bg_paths_screen_item_new
+                else -> R.drawable.bg_paths_screen_item_done
+            }
+            binding.executePendingBindings()
         }
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PathViewHolder {
-        return PathViewHolder.from(parent)
-    }
-
-    override fun onBindViewHolder(holder: PathViewHolder, position: Int) {
-        if (path != null) {
-            val score = if (scores != null && scores!!.size > position) scores!![position] else null
-            holder.bind(position, path!!.start + position, score, onClick)
-        }
-    }
-}
-
-class PathViewHolder private constructor(
-    private val binding: PathsScreenVerseItemBinding
-) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(index: Int, verse: Int, score: Score?, onClick: OnVerseClick) {
-        binding.index = index
-        binding.verse = verse.toString()
-        binding.score = score ?: Score.ZERO
-        binding.onClick = onClick
-        binding.star0 = starID(score, 1)
-        binding.star1 = starID(score, 2)
-        binding.star2 = starID(score, 3)
-        binding.background = when (score) {
-            null -> R.drawable.bg_paths_screen_item_new
-            else -> R.drawable.bg_paths_screen_item_done
-        }
-        binding.executePendingBindings()
-    }
-
-    private fun starID(score: Score?, min: Int): Int {
-        return when {
-            score != null && score.stars >= min -> R.drawable.ic_star_rate
-            else -> R.drawable.ic_star_rate_white
-        }
-    }
-
-    companion object {
-        fun from(parent: ViewGroup): PathViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val binding: PathsScreenVerseItemBinding =
-                DataBindingUtil.inflate(inflater, R.layout.paths_screen_verse_item, parent, false)
-            return PathViewHolder(binding)
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding: PathsScreenVerseItemBinding = DataBindingUtil.inflate(
+                    inflater,
+                    R.layout.paths_screen_verse_item,
+                    parent,
+                    false
+                )
+                return ViewHolder(binding)
+            }
         }
     }
 }
@@ -84,4 +76,40 @@ class OnVerseClick(
     val handler: (index: Int) -> Unit
 ) {
     fun onClick(index: Int) = handler(index)
+}
+
+data class PathVerseItem(
+    val index: Int,
+    val value: String,
+    val score: Int,
+    val star0: Int,
+    val star1: Int,
+    val star2: Int
+)
+
+private fun Path.toVerseItems(scores: List<Score>?): List<PathVerseItem> {
+    val list = mutableListOf<PathVerseItem>()
+    for (v in start..end) {
+        val index = v - start
+        val score = scores?.getOrNull(index) ?: Score.ZERO
+        val stars = score.stars
+        list.add(
+            PathVerseItem(
+                index,
+                v.toString(),
+                score.value,
+                stars.toStarID(1),
+                stars.toStarID(2),
+                stars.toStarID(3)
+            )
+        )
+    }
+    return list
+}
+
+private fun Int.toStarID(min: Int): Int {
+    return when {
+        this >= min -> R.drawable.ic_star_rate
+        else -> R.drawable.ic_star_rate_white
+    }
 }
